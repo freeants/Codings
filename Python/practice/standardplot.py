75c79bd57d61d3732a4f3aa831911bd06acc7a2a
Fogg FU <ffu@nuc.local>
2020-11-20 12:04:22 +0800

    sin & cos

--- Python/practice/standardplot.py
+++ Python/practice/standardplot.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
+# Create a list of evenly-spaced numbers over the range
+x = np.linspace(0, 12,150)
+# Plot the sin&cos of each x point
+plt.plot(x, np.sin(x), label="sin")
+plt.plot(x, np.cos(x), label="cos", linestyle="--")
+# Display the plot
+plt.title("sin & cos")
+plt.legend()
+plt.show()
