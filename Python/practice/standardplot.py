453f784d851ca9e8c845d4180aa36522e308d650
Fogg FU <ffu@nuc.local>
2020-11-20 14:45:25 +0800

    refined

--- Python/practice/standardplot.py
+++ Python/practice/standardplot.py
@@ -2,8 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Create a list of evenly-spaced numbers over the range
-x = np.linspace(0, 12,150)
+x = np.linspace(-2*np.pi, 2*np.pi, 512)
 # Plot the sin&cos of each x point
+plt.xticks([-2*np.pi, -np.pi, 0, np.pi, 2*np.pi],
+           ['-2π', '-π', '0', 'π', '2π'])
+plt.yticks([-1,  0, 1])
 plt.plot(x, np.sin(x), label="sin")
 plt.plot(x, np.cos(x), label="cos", linestyle="--")
 # Display the plot
