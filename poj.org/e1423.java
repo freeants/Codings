4d0a015c26c203962f0e779f34ae4db5ca6857b0
freeants <fogg_yang@outlook.com>
2020-12-23 16:51:40 +0800

    zhihu folder outside

---
+++ poj.org/e1423.java
@@ -0,0 +1,22 @@
+
+/*
+    File:   e1423.java - Big Number
+    Copyright:  © 2020-2025 freeants. All rights reserved.
+*/
+import java.util.*;
+import java.math.BigDecimal;
+
+public class e1423 {
+    static int m; // the integer of m!
+    static int num_digits; // number of digits
+    static Scanner sc;
+    static BigDecimal fact=1; // the value of m!
+
+    static void compFact(int n) {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
