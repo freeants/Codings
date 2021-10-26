13f80eaa78d1f180128fcb99c9c5e9c931f04a86
freeants <fogg_yang@outlook.com>
2021-10-26 08:54:18 +0800

    debian

--- Cpp/misc/rss.cc
+++ Cpp/misc/rss.cc
@@ -6,63 +6,36 @@
     Version :   v0.1 Mar 27, 2021. 
 */
 
-#include <chrono>  // for timing
-#include <fstream> // File operations
-#include <random>  // Generate random numbers
+#include <chrono>    // for timing
+#include <fstream>   // File operations
+#include <random>    // Generate random numbers
+#include <algorithm> // std::sort
 #include <iostream>
 #include <iomanip>
 #include <string>
 using namespace std;
 
-#define MAX_SIZE 1000000 // Size of data dictionary
-float *a;                // Data dictionary
+#define MAX_SIZE 1000000  // Size of data dictionary
+#define SRC_FILE "M1.txt" // Source file name
+#define DST_FILE "M2.txt" // Destination file name
+
+float *a; // Data dictionary
 
 /*
  * Lamda expression for displaying stats.
  */
-static auto dispResult = [](string str, auto diffTime) {
+static auto dispResult = [](string str, auto diffTime)
+{
     cout << left << setw(30) << str << setw(20) << chrono::duration_cast<chrono::microseconds>(diffTime).count() << endl;
 };
-
-/* 
- * quickSort()
- */
-void quickSort(float A[], int lo, int hi)
-{
-    int i = lo, j = hi;
-    int tmp;
-    float pivot = A[(lo + hi) / 2];
-
-    /* partition */
-    while (i <= j)
-    {
-        while (A[i] < pivot)
-            i++;
-        while (A[j] > pivot)
-            j--;
-        if (i <= j)
-        {
-            swap(A[i], A[j]);
-            i++;
-            j--;
-        }
-    };
-
-    /* recursion */
-    if (lo < j)
-        quickSort(A, lo, j);
-    if (i < hi)
-        quickSort(A, i, hi);
-}
 
 void FileRead(string file_name)
 {
     ifstream ifs(file_name);
-
-    string contents((istreambuf_iterator<char>(ifs)),
-                    istreambuf_iterator<char>());
-    contents.c_str();
-    
+    for (int i = 0; i < MAX_SIZE; i++)
+    {
+        ifs >> a[i];
+    }
 }
 
 void FileSave(string file_name)
@@ -70,7 +43,6 @@ void FileSave(string file_name)
     ofstream ofs(file_name);
     for (int i = 0; i < MAX_SIZE; i++)
         ofs << a[i] << endl;
-    //ofs << a;
     ofs.close();
 }
 
@@ -79,8 +51,6 @@ void FileSave(string file_name)
  */
 void BuildDataDictionary()
 {
-    cout << "Building data dictionary ... (size: " << MAX_SIZE << ") " << endl;
-
     // Define the array that holds all data
     a = new float[MAX_SIZE];
 
@@ -94,30 +64,36 @@ void BuildDataDictionary()
 
 void doTests()
 {
-    cout << "//////////////////////////////////////////////////////////////" << endl;
+    cout << "/////////////////////////////////////////////////////////////////" << endl;
     cout << left << setw(30) << "Operations" << setw(20) << "Time elapsed(μ)" << endl;
 
-    /* 1. Read from M1.txt */
+    /* 0. Generate 1M random numbers& save to file*/
+    auto t = chrono::high_resolution_clock::now(); //get start time
+    BuildDataDictionary();
+    FileSave("M1.txt");
     auto t0 = chrono::high_resolution_clock::now(); //get start time
-    FileRead("M1.txt");
+    dispResult("0. Build data & save file", t0 - t);
+
+    /* 1. Read from source file */
+    FileRead(SRC_FILE);
     auto t1 = chrono::high_resolution_clock::now(); //get start time
-    dispResult("1.File reads (M1.txt)", t1 - t0);
+    dispResult("1. File reads", t1 - t0);
 
     /* 2. Sorting the numbers */
-    quickSort(a, 0, MAX_SIZE - 1);
+    sort(a, a + MAX_SIZE);
     auto t2 = chrono::high_resolution_clock::now(); //get start time
-    dispResult("2.Sorting numbers", t2 - t1);
+    dispResult("2. Sorting", t2 - t1);
 
-    /* 3. Save the sorted numbers to file M2.txt */
-    FileSave("M2.txt");
+    /* 3. Save the sorted numbers to destination file */
+    FileSave(DST_FILE);
     auto t3 = chrono::high_resolution_clock::now(); //get start time
-    dispResult("3.File save (M2.txt)", t3 - t2);
+    dispResult("3. File save", t3 - t2);
 
     /* Print overview */
     //dispResult("Total time elapsed", t3 - t0);
-    auto timeElapsed = chrono::duration_cast<chrono::microseconds>(t3 - t0).count();
+    auto timeElapsed = chrono::duration_cast<chrono::microseconds>(t3 - t).count();
     auto timenow = chrono::system_clock::to_time_t(chrono::system_clock::now());
-    cout << "//////////////////////////////////////////////////////////////" << endl;
+    cout << "/////////////////////////////////////////////////////////////////" << endl;
     cout << left << setw(30) << " Total time" << setw(20) << timeElapsed << (double)timeElapsed / 1000000 << " seconds"
          << endl;
     cout << left << setw(30) << " Completed @ " << setw(20) << ctime(&timenow) << endl;
@@ -128,12 +104,6 @@ int main(int argc, char const *argv[])
     cout << "Read, Sort and Save 1M floating numbers (C++) ..." << endl;
     try
     {
-        /* Generate 1M random numbers */
-        BuildDataDictionary();
-
-        /* Save to file M1.txt */
-        FileSave("M1.txt");
-
         /* Start the tests */
         doTests();
     }
