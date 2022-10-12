48affe5a144bc0970ac184c8c5671e12e640e911
freeants <fogg_yang@outlook.com>
2022-10-12 23:54:27 +0800

    big integer tests

---
+++ Cpp/misc/SameBirth.c
@@ -0,0 +1,16 @@
+/*
+ * SameBirth.c - Calculate the possibility of N 
+ *               people where two share the same birthday.
+ * Pn = 1 - factorial(365) / (365**n * factorial(365-n))
+ */
+
+#include <stdio.h>
+#include <gmp.h>
+
+int main(int argc, char const *argv[])
+{
+    mpz_fac fact =1;
+    fact.
+
+    return 0;
+}
