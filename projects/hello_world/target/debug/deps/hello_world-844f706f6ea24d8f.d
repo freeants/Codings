242ec673c6122a781c0444fb07964c47a841b449
freeants <fogg_yang@outlook.com>
2024-04-20 16:51:54 +0800

    Cargo new

---
+++ projects/hello_world/target/debug/deps/hello_world-844f706f6ea24d8f.d
@@ -0,0 +1,5 @@
+/Users/ffu/Codings/projects/hello_world/target/debug/deps/libhello_world-844f706f6ea24d8f.rmeta: src/main.rs
+
+/Users/ffu/Codings/projects/hello_world/target/debug/deps/hello_world-844f706f6ea24d8f.d: src/main.rs
+
+src/main.rs:
