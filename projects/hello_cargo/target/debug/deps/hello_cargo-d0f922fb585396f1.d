7bfdd875d0367067bf02d7a105322015f02aff61
freeants <fogg_yang@outlook.com>
2024-04-20 23:16:13 +0800

    cargo projects

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-d0f922fb585396f1.d
@@ -0,0 +1,5 @@
+/Users/ffu/Codings/projects/hello_cargo/target/debug/deps/libhello_cargo-d0f922fb585396f1.rmeta: src/main.rs
+
+/Users/ffu/Codings/projects/hello_cargo/target/debug/deps/hello_cargo-d0f922fb585396f1.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/ffu/Codings/projects/hello_cargo/target/release/hello_cargo: /Users/ffu/Codings/projects/hello_cargo/src/main.rs
