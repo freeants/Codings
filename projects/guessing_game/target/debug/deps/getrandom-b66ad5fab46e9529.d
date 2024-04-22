ee2c58074befbac623786d0c519ff4a5302a0759
freeants <fogg_yang@outlook.com>
2024-04-22 21:48:42 +0800

    VS on Windowsroutine build

---
+++ projects/guessing_game/target/debug/deps/getrandom-b66ad5fab46e9529.d
@@ -0,0 +1,10 @@
+/Users/ffu/Codings/projects/guessing_game/target/debug/deps/libgetrandom-b66ad5fab46e9529.rmeta: /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/lib.rs /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/error.rs /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/util.rs /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/error_impls.rs /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/util_libc.rs /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/macos.rs
+
+/Users/ffu/Codings/projects/guessing_game/target/debug/deps/getrandom-b66ad5fab46e9529.d: /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/lib.rs /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/error.rs /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/util.rs /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/error_impls.rs /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/util_libc.rs /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/macos.rs
+
+/Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/lib.rs:
+/Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/error.rs:
+/Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/util.rs:
+/Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/error_impls.rs:
+/Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/util_libc.rs:
+/Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/getrandom-0.2.14/src/macos.rs:
---
+++ projects/guessing_game/target/debug/deps/guessing_game-ba35d4f40d875a25.d
@@ -0,0 +1,5 @@
+/Users/ffu/Codings/projects/guessing_game/target/debug/deps/libguessing_game-ba35d4f40d875a25.rmeta: src/main.rs
+
+/Users/ffu/Codings/projects/guessing_game/target/debug/deps/guessing_game-ba35d4f40d875a25.d: src/main.rs
+
+src/main.rs:
