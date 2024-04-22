30c2c04727cf5265756b3b801b368aa676795c36
freeants <fogg_yang@outlook.com>
2024-04-22 21:48:42 +0800

    routine build

---
+++ projects/guessing_game/target/debug/deps/cfg_if-7054ea7881b1d967.d
@@ -0,0 +1,5 @@
+/Users/ffu/Codings/projects/guessing_game/target/debug/deps/libcfg_if-7054ea7881b1d967.rmeta: /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
+
+/Users/ffu/Codings/projects/guessing_game/target/debug/deps/cfg_if-7054ea7881b1d967.d: /Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
+
+/Users/ffu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
