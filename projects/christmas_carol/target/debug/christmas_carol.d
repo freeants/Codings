db6e6f9b532be0dfede94b44694fa21dcf833bce
freeants <fogg_yang@outlook.com>
2024-04-30 06:19:32 +0800

    vs code

--- projects/christmas_carol/target/debug/christmas_carol.d
+++ projects/christmas_carol/target/debug/christmas_carol.d
@@ -1,1 +1,1 @@
-C:\Users\FFU\source\repos\freeants\Codings\projects\christmas_carol\target\debug\christmas_carol.exe: C:\Users\FFU\source\repos\freeants\Codings\projects\christmas_carol\src\main.rs
+/Users/ffu/Codings/Codings/projects/christmas_carol/target/debug/christmas_carol: /Users/ffu/Codings/Codings/projects/christmas_carol/src/main.rs
---
+++ projects/christmas_carol/target/debug/deps/christmas_carol-fd905cee749c72a5.d
@@ -0,0 +1,5 @@
+/Users/ffu/Codings/Codings/projects/christmas_carol/target/debug/deps/christmas_carol-fd905cee749c72a5: src/main.rs
+
+/Users/ffu/Codings/Codings/projects/christmas_carol/target/debug/deps/christmas_carol-fd905cee749c72a5.d: src/main.rs
+
+src/main.rs:
