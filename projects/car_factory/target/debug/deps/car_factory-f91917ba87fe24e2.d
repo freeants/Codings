066d3be1a23402d2409ce6b890a14309666724a0
freeants <fogg_yang@outlook.com>
2024-05-02 02:17:08 +0800

    new practice

---
+++ projects/car_factory/target/debug/deps/car_factory-f91917ba87fe24e2.d
@@ -0,0 +1,5 @@
+/Users/ffu/Codings/projects/car_factory/target/debug/deps/car_factory-f91917ba87fe24e2: src/main.rs
+
+/Users/ffu/Codings/projects/car_factory/target/debug/deps/car_factory-f91917ba87fe24e2.d: src/main.rs
+
+src/main.rs:
