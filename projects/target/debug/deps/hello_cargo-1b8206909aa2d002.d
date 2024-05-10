dd6166c0d91333470bdbdf934d0ff025b818e379
freeants <fogg_yang@outlook.com>
2024-05-10 13:23:21 +0800

    vs code updated

---
+++ projects/target/debug/deps/hello_cargo-1b8206909aa2d002.d
@@ -0,0 +1,5 @@
+C:\Users\FFU\source\repos\freeants\Codings\projects\target\debug\deps\libhello_cargo-1b8206909aa2d002.rmeta: hello_cargo\src\main.rs
+
+C:\Users\FFU\source\repos\freeants\Codings\projects\target\debug\deps\hello_cargo-1b8206909aa2d002.d: hello_cargo\src\main.rs
+
+hello_cargo\src\main.rs:
---
+++ projects/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\FFU\source\repos\freeants\Codings\projects\target\debug\deps\hello_cargo.exe: hello_cargo\src\main.rs
+
+C:\Users\FFU\source\repos\freeants\Codings\projects\target\debug\deps\hello_cargo.d: hello_cargo\src\main.rs
+
+hello_cargo\src\main.rs:
