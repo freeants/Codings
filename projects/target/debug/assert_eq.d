62d745a43e5d139fdb37943867be78ec9e204d65
freeants <fogg_yang@outlook.com>
2024-05-14 07:59:15 +0800

    test

--- projects/target/debug/assert_eq.d
+++ projects/target/debug/assert_eq.d
@@ -1,1 +1,1 @@
-C:\Users\FFU\source\repos\freeants\Codings\projects\target\debug\assert_eq.exe: C:\Users\FFU\source\repos\freeants\Codings\projects\assert_eq\src\main.rs
+/Users/ffu/Codings/projects/target/debug/assert_eq: /Users/ffu/Codings/projects/assert_eq/src/main.rs
---
+++ projects/target/debug/deps/assert_eq-7715645de48308d6.d
@@ -0,0 +1,5 @@
+/Users/ffu/Codings/projects/target/debug/deps/assert_eq-7715645de48308d6: assert_eq/src/main.rs
+
+/Users/ffu/Codings/projects/target/debug/deps/assert_eq-7715645de48308d6.d: assert_eq/src/main.rs
+
+assert_eq/src/main.rs:
