ece247f2134066067fb478f5704383f8e9db2e8d
freeants <fogg_yang@outlook.com>
2024-10-04 22:24:53 +0800

    cargo clean

---
+++ Algs04/exercises/exp_val.java
@@ -0,0 +1,15 @@
+/*
+1.1.1 Give the value of each of the following expressions:
+a. ( 0 + 15 ) / 2
+b. 2.0e-6 * 100000000.1
+c. true && false || true && true
+ */
+package exercises;
+
+public class exp_val {
+    public static void main(String[] args) {
+        System.out.println((0 + 15) / 2);
+        System.out.println(2.0e-6 * 100000000.1);
+        System.out.println(true && false || true && true);
+    }
+}
