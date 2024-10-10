18e6e64d07b88dc9f61df29fd42dbdae336faa31
freeants <fogg_yang@outlook.com>
2024-10-10 20:26:47 +0800

    new prog

---
+++ Cpp/misc/isTrue.c
@@ -0,0 +1,23 @@
+/*
+    isTrue.c - Test to see '1<0<3' is TRUE or FALSE.
+    Author: freeants, All rights reserved.
+*/
+
+#include <stdio.h>
+// #include <stdbool.h> // for bool type in C
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+
+    // if ((1 < 0 < 3))
+    //{
+    //     printf("TRUE\n");
+    // }
+    // else
+    //{
+    //     printf("FALSE\n");
+    // }
+    printf("%d\n", (1 < 0 < 3));
+    return 0;
+}
