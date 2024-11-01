a282d626f4c9695b8a351d12fc0f96153ddb1b6d
freeants <fogg_yang@outlook.com>
2024-11-01 19:41:53 +0800

    sync

---
+++ projects/target/debug/deps/build_a_car-bcf32f3e0d867610.d
@@ -0,0 +1,5 @@
+/Users/ffu/source/repos/Codings/projects/target/debug/deps/libbuild_a_car-bcf32f3e0d867610.rmeta: build_a_car/src/main.rs
+
+/Users/ffu/source/repos/Codings/projects/target/debug/deps/build_a_car-bcf32f3e0d867610.d: build_a_car/src/main.rs
+
+build_a_car/src/main.rs:
